7aa90e2abd1b1ca094813194a8207d82d5d7dcf3
Jugurtha Kacimi <jugurthakacimi1@gmail.com>
2024-09-11 12:29:38 -0500

    Added Sql Server DB to project

--- Program.cs
+++ Program.cs
@@ -9,12 +9,26 @@ using Microsoft.Data.SqlClient;
 using (var connection = new SqlConnection(Variables.defaultConnection))
 {
     var sql =
-        @"CREATE TABLE IF NOT EXISTS coding_tracker (
-                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        StartTime TEXT,
-                        EndTime TEXT,
-                        Duration INTEGER
-                        )";
+        @"
+    IF OBJECT_ID('Stack', 'U') IS NULL
+    BEGIN
+        CREATE TABLE Stack (
+            Id INT IDENTITY(1,1) PRIMARY KEY,
+            Name NVARCHAR(255) NOT NULL
+        );
+    END;
+
+    IF OBJECT_ID('Flashcard', 'U') IS NULL
+    BEGIN
+        CREATE TABLE Flashcard (
+            Id INT IDENTITY(1,1) PRIMARY KEY,
+            Front NVARCHAR(255) NOT NULL,
+            Back NVARCHAR(255) NOT NULL,
+            StackId INT,
+            FOREIGN KEY (StackId) REFERENCES Stack(Id)
+        );
+    END;
+";
 
     connection.Execute(sql);
 }
